02c60537bb9c06c1421aecd12988daac02f59f32
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-26 15:28:50 -0700

    fix: Make sure testing for Markets Api get all matches serializer output

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -9,30 +9,32 @@ describe "Markets Api" do
       expect(response).to be_successful
 
       markets = JSON.parse(response.body, symbolize_names: true)
-
-      markets.each do |market|
+      markets[:data].each do |market|
         expect(market).to have_key(:id)
-        expect(market).to have_key(:name)
-        expect(market).to have_key(:street)
-        expect(market).to have_key(:city)
-        expect(market).to have_key(:county)
-        expect(market).to have_key(:state)
-        expect(market).to have_key(:zip)
-        expect(market).to have_key(:lat)
-        expect(market).to have_key(:lon)
-        expect(market).to have_key(:vendor_count)
-        
-        expect(market[:name]).to be_an(String)
-        expect(market[:street]).to be_an(String)
-        expect(market[:city]).to be_an(String)
-        expect(market[:county]).to be_an(String)
-        expect(market[:state]).to be_an(String)
-        expect(market[:zip]).to be_an(String)
-        expect(market[:lat]).to be_an(String)
-        expect(market[:lon]).to be_an(String)
-        expect(market[:vendor_count]).to be_an(Integer)
+        expect(market).to have_key(:type)
+        expect(market).to have_key(:attributes)
+        expect(market[:attributes]).to have_key(:name)
+        expect(market[:attributes]).to have_key(:street)
+        expect(market[:attributes]).to have_key(:city)
+        expect(market[:attributes]).to have_key(:county)
+        expect(market[:attributes]).to have_key(:state)
+        expect(market[:attributes]).to have_key(:zip)
+        expect(market[:attributes]).to have_key(:lat)
+        expect(market[:attributes]).to have_key(:lon)
+        expect(market[:attributes]).to have_key(:vendor_count)
 
-
+        expect(market[:id]).to be_a(String)
+        expect(market[:type]).to be_a(String)
+        expect(market[:attributes]).to be_a(Hash)
+        expect(market[:attributes][:name]).to be_a(String)
+        expect(market[:attributes][:street]).to be_a(String)
+        expect(market[:attributes][:city]).to be_a(String)
+        expect(market[:attributes][:county]).to be_a(String)
+        expect(market[:attributes][:state]).to be_a(String)
+        expect(market[:attributes][:zip]).to be_a(String)
+        expect(market[:attributes][:lat]).to be_a(String)
+        expect(market[:attributes][:lon]).to be_a(String)
+        expect(market[:attributes][:vendor_count]).to be_an(Integer)
       end
     end
 end
