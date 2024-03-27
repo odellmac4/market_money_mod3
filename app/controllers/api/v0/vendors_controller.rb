dde6a959f08e86d50554d02f351bdacd592da255
Odell McFarland IV <odell.mac4@gmail.com>
2024-03-26 21:19:41 -0700

    Fix: add comment

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -7,6 +7,7 @@ class Api::V0::VendorsController < ApplicationController
   def create
     vendor = Vendor.new(vendor_params)
     if vendor.save
+      # These statuses are explicitly returning the status responses we want based on requirements
       render json: VendorSerializer.new(vendor), status: :created
     else
       render json: { errors: vendor.errors.full_messages }, status: :bad_request
