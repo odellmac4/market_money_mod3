78f9569b326f94a17837e58475eb19ef0419c43c
Odell McFarland IV <odell.mac4@gmail.com>
2024-03-27 15:04:48 -0700

    Test: create validation and test validations for all attributes with a custom validation

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,15 @@
 class Vendor < ApplicationRecord
-has_many :market_vendors, dependent: :destroy
+  has_many :market_vendors, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :contact_name, presence: true
+  validates :contact_phone, presence: true
+  validate :validate_credit_accepted
+
+  def validate_credit_accepted
+    if credit_accepted.nil? || ![true, false].include?(credit_accepted)
+      errors.add(:credit_accepted, "must be true or false")
+    end
+  end
 end
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -4,4 +4,39 @@ describe Vendor do
   describe "relationships" do
     it { should have_many(:market_vendors) }
   end
+
+  describe "validations" do
+    # before(:each) do
+    #   @vendor_1 = create(:vendor, credit_accepted: true)
+    #   @vendor_2 = create(:vendor, credit_accepted: false)
+    #   
+    # end
+    
+
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:contact_name)}
+    it {should validate_presence_of(:contact_phone)}
+
+    it 'validates that credit_accepted is boolean' do
+      should validate_inclusion_of(:credit_accepted).
+        in_array([true, false]).
+        with_message('must be true or false')
+    end 
+
+    it 'raises error when credit_accepted is nil' do
+
+      expect{
+        Vendor.create!(
+        {
+          name: "Luff",
+          description: "streetwear",
+          contact_name: "E Hob",
+          contact_phone: "6302720865",
+          credit_accepted: nil
+        }
+      )
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
