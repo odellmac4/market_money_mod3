94befe9e736d01d41882b15a108f3b8303f75777
Odell McFarland IV <odell.mac4@gmail.com>
2024-03-26 19:42:08 -0700

    Routes: add post route to create a new vendor

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :markets, only: [:index, :show]
-      resources :vendors, only: [:show, :destroy]
+      resources :vendors, only: [:show, :destroy, :create]
     end
   end
 end
