051e36f751385278aa80a76009ca6ed5813f444e
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-27 17:47:34 -0700

    fix: Make sure routes are correct

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,7 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :markets, only: [:index, :show]
       resources :vendors, only: [:show, :destroy, :create]
-      resources :vendors, only: [:show, :destroy]
       resources :market_vendors, only: [:create]
-      resources :vendors, only: [:show, :destroy, :create]
     end
   end
 end
