ba8b7b3e3db00622b761a66f21c2c71f78bee899
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-28 14:12:45 -0700

    fix: Make sure spacing is aligned for test

--- spec/services/atm_service_spec.rb
+++ spec/services/atm_service_spec.rb
@@ -1,14 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "AtmService" do
-  it "gets a connection", :vcr do
+  it "gets a response", :vcr do
     service = AtmService.new
-
     lat = 38.9169984
     lon = -77.0320505
-
     data = service.get_response(lat, lon)
     summary_keys = [:query, :queryType, :queryTime, :numResults, :offset, :totalResults, :fuzzyLevel, :geoBias]
+
     expect(data).to be_a(Hash)
     expect(data.keys).to eq([:summary, :results])
     expect(data[:summary]).to be_a(Hash)
@@ -16,6 +15,7 @@ RSpec.describe "AtmService" do
     expect(data[:results]).to be_an(Array)
 
     result_keys = [:type, :id, :score, :dist, :info, :poi, :address, :position, :viewport, :entryPoints,]
+
     data[:results].each do |result|
       expect(result.keys).to eq(result_keys)
       expect(result[:dist]).to be_a(Float)
