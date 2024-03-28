59dd5be756a31e740d5172da9e2bae872661de39
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-27 20:48:32 -0700

    fix: Remove unused code

--- app/poros/error_message.rb
+++ app/poros/error_message.rb
@@ -5,8 +5,4 @@ class ErrorMessage
     @message = message
     @status_code = status_code
   end
-
-  # def object_must_exist
-  #   "#{@message.split[2]} must exist"
-  # end
 end
--- spec/poros/error_message_spec.rb
+++ spec/poros/error_message_spec.rb
@@ -11,14 +11,4 @@ describe "Error Message PORO" do
     expect(error_message.message).to eq("Error")
     expect(error_message.status_code).to eq(404)
   end
-
-  # describe "instance methods" do
-  #   describe "#object_must_exist" do
-  #     it "turns a Hash message to a String" do
-  #     error_message = ErrorMessage.new("Couldn't find Market with 'id'=1", 404)
-
-  #     expect(error_message.object_must_exist).to eq(["Market must exist"])
-  #     end
-  #   end
-  # end
 end
