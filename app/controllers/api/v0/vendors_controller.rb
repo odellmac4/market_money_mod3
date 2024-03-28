548aafe8c4eeeec2970034a28f1424f80218b374
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-28 12:16:28 -0700

    fix: Add status codes to MarketVendors#destroy, and Vendors#show

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -6,7 +6,7 @@ class Api::V0::VendorsController < ApplicationController
 
   def show
     vendor = Vendor.find(params[:id])
-    render json: VendorSerializer.new(vendor)
+    render json: VendorSerializer.new(vendor), status: :ok
   end
 
   def create
