c70f51ffd9751d9009919c303b0abeaddc08d074
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-28 16:30:16 -0700

    fix: Change @facade to @atm_facade to be more explicit

--- app/controllers/api/v0/nearest_atms_controller.rb
+++ app/controllers/api/v0/nearest_atms_controller.rb
@@ -1,7 +1,7 @@
 class Api::V0::NearestAtmsController < ApplicationController
   def index
     market = Market.find(params[:market_id])
-    @facade = AtmFacade.new(market.lat, market.lon)
+    @atm_facade = AtmFacade.new(market.lat, market.lon)
 
     render json: AtmSerializer.new(@facade.sorted_atms).serialize_data, status: :ok
   end
