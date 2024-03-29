6a150569b0f042d1470e8a19e8d3b4c45f77e32d
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-28 20:20:55 -0700

    fix: Make sure to not set id for the atm attribute

--- app/poros/atm.rb
+++ app/poros/atm.rb
@@ -2,7 +2,6 @@ class Atm
   attr_reader :name, :address, :lat, :lon, :distance, :id
 
   def initialize(data)
-    @id = nil
     @name = data[:poi][:name]
     @address = data[:address][:freeformAddress]
     @lat = data[:position][:lat]
