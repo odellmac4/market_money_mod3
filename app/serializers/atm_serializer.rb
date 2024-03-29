52db1bcdb7825a179775b916c31e02b5e51db690
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-28 19:37:04 -0700

    fix: Remove unused code

--- app/serializers/atm_serializer.rb
+++ app/serializers/atm_serializer.rb
@@ -1,31 +1,4 @@
 class AtmSerializer
   include JSONAPI::Serializer
     attributes :name, :address, :lat, :lon, :distance, :id
-
-    # attribute :active do
-    #   true
-    # end
-
-  # def initialize(atm_poro)
-  #   @atm = atm_poro
-  # end
-
-  # def serialize_data
-  #   {
-  #     data:
-  #       @atm.map do |atm|
-  #         {
-  #           id: nil,
-  #           type: "atm",
-  #           attributes: {
-  #             name: atm.name,
-  #             address: atm.address,
-  #             lat: atm.lat,
-  #             lon: atm.lon,
-  #             distance: atm.distance
-  #           }
-  #         }
-  #       end
-  #     }
-  # end
 end
